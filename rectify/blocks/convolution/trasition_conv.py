fa575ba42d0d25400c5af4f12fcaac227a0b666c
hanmt <hanmt@avtvn.com>
2025-10-31 22:07:40 +0700

    hard norm and act

--- rectify/blocks/convolution/trasition_conv.py
+++ rectify/blocks/convolution/trasition_conv.py
@@ -2,18 +2,17 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from .base import BaseConvBlock 
-
 class TransitionConvBlock(BaseConvBlock):
-    def __init__(self, in_channels, out_channels, normalization='bn', norm_params={}, activation='relu', act_params={}):
+    def __init__(self, in_channels, out_channels, droprate=0.0):
         super(TransitionConvBlock, self).__init__()
-        if normalization not in self.norm_dict.keys():
-            raise ValueError(f"Unsupported normalization type: {normalization}")
-        if activation not in self.act_dict.keys():
-            raise ValueError(f"Unsupported activation type: {activation}")
-        self.norm = self.norm_dict[normalization](in_channels, **norm_params)
-        self.activation = self.act_dict[activation](**act_params)
+        NormLayer = nn.BatchNorm2d
+        ActLayer = nn.ReLU
+        self.norm = NormLayer(in_channels)
+        self.activation = ActLayer(inplace=True)
         self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=1, padding=0, bias=False)
-        self.droprate = 0.0
+        
+        self.droprate = droprate
+        
     def forward(self, x):
         out = self.conv1(self.activation(self.norm(x)))
         if self.droprate > 0:
